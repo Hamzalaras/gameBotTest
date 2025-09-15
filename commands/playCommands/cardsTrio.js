941bcd76edf8483b78aa11b55731fdae7802d381
Hamza Laras <hamzalaras44@gmail.com>
2025-09-15 22:36:39 +0100

    wow

--- commands/playCommands/cardsTrio.js
+++ commands/playCommands/cardsTrio.js
@@ -1,5 +1,7 @@
 const {  } = require('discord.js');
+const { Management } = require('../../dataBase.js');
 const { ErrorUnit, FalseInput } = require('../../centralUnits/errorUnit.js');
+const cardsJson = require('../../data/cards/cards.json');
 
 module.exports = {
     name: ['تشكيلة', 'مجموعة'],
@@ -7,10 +9,21 @@ module.exports = {
     need: true,
     async execute(msg, args){
         try {
-            if(!args[1]){
+            const keyWord = ['هجوم', 'دفاع'].some(kw => kw === args[1]) ?
+                            args[1] :
+                            undefined;
+            if(!keyWord){
                 throw new FalseInput('تشكيلة');
             }
-            
+
+            const userTeam = (await Management.selectManager(['first_card_id', 'second_card_id', 'third_card_id'], `players_team_${keyWord}`, 'player_id', msg.author.id))[0];
+            const [firstId, secondId, thirdId] = [userTeam.first_card_id, userTeam.second_card_id, userTeam.third_card_id];
+            const [firstCard, secondCard, thirdCard] = [ , , ];
+            for(const type of cardsJson){
+                const cards = type.cards;
+                for(const card of cards){
+                }
+            }
         } catch (error) {
             
         }
